4fa35a999a7d755204cd2df9221f64e431a23afe
Ajluces10 <mr.andrewjulao@gmail.com>
2023-03-07 23:49:55 +0800

    e-atm deployment

---
+++ main.js
@@ -0,0 +1,12 @@
+console.log('Hello World!');
+//let open = document.getElementById("show").style.display="block";
+function closenav() {
+  document.getElementById("show").style.display="none";
+ // alert("hello");
+}
+function opennav() {
+   document.getElementById("show").style.display="block";
+}
+function submit() {
+  alert("login successful!");
+}
